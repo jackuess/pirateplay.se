befa511ac9d9e07dc399f4536f15907e474c4de7
Jacques de Laval <chucky@wrutschkow.org>
2012-08-31 02:02:28 +0200

    First commit!

---
+++ app.wsgi
@@ -0,0 +1,140 @@
+import cherrypy, json, re, sys
+from copy import deepcopy
+
+from genshi.template import TemplateLoader
+from genshi.core import Markup
+
+class JSONPirateEncoder(json.JSONEncoder):
+	def default(self, o):
+		try:
+			return o.to_dict()
+		except AttributeError:
+			pass
+		return JSONEncoder.default(self, o)
+
+class Root(object):
+	def __init__(self, template_dir):
+		self._tmpl_loader = TemplateLoader(template_dir, auto_reload=True)
+		self._jsencoder = JSONPirateEncoder()
+		self._templates = {}
+
+	def _render_template(self, template_name, args = {}, type = 'xhtml'):
+		try:
+			self._templates[template_name]
+		except KeyError:
+			self._templates[template_name] = self._tmpl_loader.load(template_name)
+		return self._templates[template_name].generate(**args).render(type)
+
+	@cherrypy.expose
+	def get_streams_js(self, url = 'http://www.tv4play.se/sport/ekwall_vs_lundh?title=ekwall_vs_lundh_med_may_mahlangu&videoid=2095439', rnd = None):
+		cherrypy.response.headers['Content-Type'] = 'application/json'
+		return self._jsencoder.encode(get_streams(url))
+
+	@cherrypy.expose
+	def get_streams_xml(self, url = 'http://www.tv4play.se/sport/ekwall_vs_lundh?title=ekwall_vs_lundh_med_may_mahlangu&videoid=2095439', rnd = None):
+		cherrypy.response.headers['Content-Type'] = 'application/xml'
+		
+		return self._render_template(template_name = 'get_streams.xml',
+									args = dict(streams = [s.to_dict()
+											for s in get_streams(url)]),
+									type = 'xml')
+	
+	@cherrypy.expose
+	def get_streams_old_xml(self, url = 'http://www.tv4play.se/sport/ekwall_vs_lundh?title=ekwall_vs_lundh_med_may_mahlangu&videoid=2095439', rnd = None):
+		cherrypy.response.headers['Content-Type'] = 'application/xml'
+		
+		return self._render_template(template_name = 'get_streams_old.xml',
+									args = dict(streams = [s.to_dict()
+											for s in get_streams(url)]),
+									type = 'xml')
+
+	def _convert_service_re(self, service):
+		s = deepcopy(service)
+		try:
+			s.items[0].re = re.sub(r'(\(\?P<\w+>)|(\(\?:)', '(', service.items[0].re)
+		except IndexError:
+			return service
+		else:
+			return s
+		
+	def _filter_services(self, titles):
+		try:
+			titles = [t.lower() for t in titles.split(',')]
+		except AttributeError:
+			return services
+		else:
+			return [s for s in services if s.title.lower() in titles]
+	
+	@cherrypy.expose
+	def services_js(self, titles = None, rnd = ''):
+		cherrypy.response.headers['Content-Type'] = 'application/json'
+		
+		s = self._filter_services(titles)
+		s = [self._convert_service_re(z) for z in s]
+		
+		return self._jsencoder.encode(s)
+
+	@cherrypy.expose
+	def services_xml(self, titles = None, rnd = ''):
+		cherrypy.response.headers['Content-Type'] = 'application/xml'
+		
+		return self._render_template(template_name = 'services.xml',
+									args = dict(services = [self._convert_service_re(s).to_dict()
+											for s in self._filter_services(titles)]),
+									type = 'xml')
+	
+	@cherrypy.expose
+	def index_html(self):
+		return self._render_template(template_name = 'index.html')
+	
+	@cherrypy.expose
+	def app_html(self):
+		return self._render_template(template_name = 'app.html')
+	
+	@cherrypy.expose
+	def api_html(self):
+		return self._render_template(template_name = 'api.html')
+	
+	@cherrypy.expose
+	def qna_html(self):
+		qna_txt = open('qna.txt', 'r')
+		qna = dict([pair.split('<!-- inner_delim -->') for pair in qna_txt.read().decode('utf-8').split('<!-- delim -->')])
+		qna = dict([(Markup(q.strip()), Markup(a.strip())) for q, a in qna.items()])
+		
+		return self._render_template(template_name = 'qna.html', args = {'qna': qna})
+
+
+_config = { 'global': {
+			'server.environment': 'production',
+			'server.socket_host': '0.0.0.0',
+			'server.socket_port': 8080 },
+		'/': {
+			'tools.encode.on': True,
+			'tools.encode.encoding': 'utf8' },
+		'/static': {
+			'tools.staticdir.on': True,
+			'tools.staticdir.dir': 'static' } }
+
+def application(environ, start_response):
+	from os import chdir
+	base_dir = environ.get('pirateplay_base_dir', '')
+	chdir(base_dir)
+	
+	if not base_dir in sys.path:
+		sys.path.append(base_dir)
+	global get_streams, services
+	from lib.main import get_streams, services
+	
+	_config['/']['tools.staticdir.root'] = base_dir
+	cherrypy.tree.mount(Root(template_dir = base_dir + '/templates'), config = _config, script_name = environ.get('pirateplay_script_name', ''))
+	return cherrypy.tree(environ, start_response)
+
+if __name__ == "__main__":
+	from lib.main import get_streams, services
+	
+	from os import getcwd
+	base_dir = getcwd()
+	
+	_config['global']['server.socket_port'] = 8081
+	_config['/']['tools.staticdir.root'] = base_dir
+	cherrypy.quickstart(Root(template_dir = base_dir + '/templates'), config = _config, script_name='/pirateplay2')
---
+++ static/app.js
@@ -0,0 +1,159 @@
+function rtmpDumpCmd(url, out) {
+	return 'rtmpdump -r  -o "' + out + '" ' +
+			(url.replace(/playpath=/, '-y ')
+				.replace(/swfVfy=1 swfUrl=/, '-W ')
+				.replace(/app=/, '-a ')
+				.replace(/live=1/, '-v '));
+}
+
+function stripExt(s) {
+	return s.lastIndexOf('.') == -1 ? s : s.substr(0, s.lastIndexOf('.'));
+}
+
+function messages() {
+	this.push = function (msg, type) {
+		if (typeof type === 'undefined')
+			var r = $('<li>' + msg + '</li>');
+		else
+			var r = $('<li class=' + type + '>' + msg + '</li>');
+		var close = $('<span class="close">Stäng</span>');
+		close.click(function () { $(this).parent().remove(); });
+		r.prepend(close);
+		$('#messages').prepend(r);
+	};
+	this.flush = function () {
+		$('#messages *').remove();
+	};
+}
+
+function streamRepresentation(stream) {
+	this.stream = stream;
+
+	this.render = function (args) {
+		var r = $('#result');
+		r.show();
+		$('#result_form').show();
+
+		if ('quality' in this.stream.meta)
+			r.append('<h2>Kvalitet: <em>' + this.stream.meta.quality + '</em></h2>')
+		if ('subtitles' in this.stream.meta && this.stream.meta.subtitles != '')
+			var subLink = '<a href="' + this.stream.meta.subtitles + '">' + this.stream.meta.subtitles + '</a>';
+		
+		if (args.show_inputs && (args.mode == 'download' || args.mode == 'play'))
+			var streamLink = this.stream.url
+		else
+			var streamLink = '<a href="' + this.stream.url + '">' + this.stream.url + '</a>';
+		
+		if (args.mode == 'download') {
+			if (this.stream.url.match(/^rtmpe?:\/\//) != null)
+				cmd = rtmpDumpCmd(this.stream.url, args.out);
+			else if (this.stream.url.match(/\.m3u8/) != null)
+				cmd = 'ffmpeg -i "' + streamLink + '" -acodec copy -vcodec copy -bsf aac_adtstoasc "' + args.out + '"';
+			else if (this.stream.url.match(/manifest\.f4m/) != null)
+				cmd = 'php AdobeHDS.php --delete --manifest "' + streamLink + '" --outfile "' + args.out + '"';
+			else
+				cmd = 'wget -O "' + args.out + '" "' + streamLink + '"';
+			console.log(args.show_inputs);
+			if (args.show_inputs)
+				r.append('<input type="text" class="cmd" value=\'' + cmd + "' />");
+			else
+				r.append('<p class="cmd">' + cmd + '</p>');
+			if (subLink) {
+				subCmd = 'wget -O "' + stripExt(args.out) + '.srt" "' + subLink + '"'
+				if (args.show_inputs)
+					r.append('<input type="text" class="cmd" value=\'' + subCmd + "' />");
+				else
+					r.append('<p class="cmd">' + subCmd + '</p>');
+			}
+		}
+		else if (args.mode == 'show') {
+			r.append('<p>Videoström: ' + streamLink + '</p>');
+			if (subLink)
+			r.append('<p>Undertexter: ' + subLink + '</p>');
+		}
+		else if (args.mode == 'play') {
+			if (args.show_inputs)
+				r.append('<input type="text" class="cmd" value=\'' + args.player + ' "' + this.stream.url + '"\' />');
+			else
+				r.append('<p class="cmd">' + args.player + ' "' + streamLink + '"</p>');
+		}
+	}
+}
+
+function streamCollection(streams) {
+	this.currStreams = [];
+	var _this = this;
+	$(streams).each(function (i, s) {
+		_this.currStreams.push(new streamRepresentation(s));;
+	});
+
+	this.render = function (args) {
+		$('#result :not(#close)').remove();
+		$(this.currStreams).each(function (i, s) {
+			s.render(args);
+		});
+	}
+}
+
+function getForm() {
+	var args = {};
+	args.out = $('#out').val();
+	args.mode = $('input[name="action"]:checked').val();
+	args.show_inputs = $('input[name="show_inputs"]:checked').length > 0;
+	if (args.mode == 'play')
+		args.player = $('#player').val();
+	return args;
+}
+
+function onStreams(streams) {
+	myStreamCollection = new streamCollection(streams);
+	var countingWords = ['Noll', 'En', 'Två', 'Tre', 'Fyra', 'Fem', 'Sex', 'Sju', 'Åtta', 'Nio', 'Tio', 'Elva', 'Tolv', 'Tretton'];
+
+	myMessages.flush();
+	myMessages.push((streams.length < 14 ? countingWords[streams.length] : streams.length) + ' ' + (streams.length == 1 ? 'ström funnen!' : 'strömmar funna!'));
+	myStreamCollection.render(getForm());
+}
+
+function requestStreams(e) {
+	var url = $('#url').val();
+
+	if (pattern.test(url)) {
+		$.get('get_streams.js', { url: url, rnd: + $.now() }, onStreams);
+		myMessages.push('Laddar...');
+	}
+	else
+		myMessages.push('Ogiltig address!', 'error');
+
+	e.preventDefault();
+}
+
+function setPattern(services) {
+	p = '';
+	$(services).each(function (i, s) {
+		if ('test' in s)
+			p += '(' + s.test + ')|';
+	});
+	pattern = new RegExp(p.slice(0, -1));
+}
+
+var myStreamCollection = {render: function () {;}};
+var myMessages = new messages();
+var pattern = new RegExp('^.*');
+
+$(document).ready(function () {
+	$.get('services.js', { rnd: $.now() }, setPattern);
+	$('form:first').submit(requestStreams);
+	$('#out').keyup(function () { myStreamCollection.render(getForm()); });
+	$('#player').click(function () { myStreamCollection.render(getForm()); });
+	$('#show_inputs').click(function () { myStreamCollection.render(getForm()); });
+	$('#close').click(function () { $('#result').hide(); $('#result_form').hide(); });
+	$('input[name="action"]').click(function () {
+		if ($(this).val() == 'show')
+			{ $('.out').hide(); $('.player').hide(); $('.download').hide(); }
+		else if ($(this).val() == 'download')
+			{ $('.out').show(); $('.player').hide(); $('.download').show(); }
+		else if ($(this).val() == 'play')
+			{ $('.out').hide(); $('.player').show(); $('.download').show(); }
+		myStreamCollection.render(getForm());
+	});
+});
