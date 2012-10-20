81e196952208f40fddf6b350709b086c4f0778f7
Jacques de Laval <chucky@wrutschkow.org>
2012-10-20 18:11:32 +0200

    Small Tv4 issue fixed

--- lib/pirateplay/services.py
+++ lib/pirateplay/services.py
@@ -129,7 +129,7 @@ tv4play = RequestChain(title = 'TV4-play', url = 'http://tv4play.se/', feed_url 
 							is_last = True)])
 tv4play_hds = RequestChain(
 				items = [TemplateRequest(
-							re = r'(http://)?(www\.)?tv4play\.se/.*(videoid|vid)=(?P<id>\d+).*',
+							re = r'(http://)?(www\.)?tv4play\.se/.*(videoid|video_id|vid)=(?P<id>\d+).*',
 							url_template = 'http://premium.tv4play.se/api/web/asset/%(id)s/play'),
 						TemplateRequest(
 							re = r'<bitrate>(?P<bitrate>[0-9]+)</bitrate>.*?<url>(?P<url>http://.*?\.mp4\.csmil/manifest\.f4m)</url>',
@@ -138,7 +138,7 @@ tv4play_hds = RequestChain(
 							is_last = True)])
 tv4play_http = RequestChain(
 				items = [TemplateRequest(
-							re = r'(http://)?(www\.)?tv4play\.se/.*(videoid|vid)=(?P<id>\d+).*',
+							re = r'(http://)?(www\.)?tv4play\.se/.*(videoid|video_id|vid)=(?P<id>\d+).*',
 							url_template = 'http://premium.tv4play.se/api/web/asset/%(id)s/play'),
 						TemplateRequest(
 							re = r'<url>(?P<url>http://[^<]+).*?(?=.*?(?P<sub>http://((anytime)|(prima))\.tv4(play)?\.se/multimedia/vman/smiroot/[^<]+))?',
@@ -147,7 +147,7 @@ tv4play_http = RequestChain(
 							is_last = True)])
 fotbollskanalen = RequestChain(title = 'Fotbollskanalen', url = 'http://fotbollskanalen.se/', sample_url = 'http://www.fotbollskanalen.se/video/?videoid=2194841',
 				items = [ TemplateRequest(
-							re = r'(http://)?(www\.)?fotbollskanalen\.se/.*(videoid|vid)=(?P<id>\d+).*',
+							re = r'(http://)?(www\.)?fotbollskanalen\.se/.*(videoid|video_id|vid)=(?P<id>\d+).*',
 							url_template = 'http://premium.tv4play.se/api/web/asset/%(id)s/play'),
 						TemplateRequest(
 							re = r'(<playbackStatus>(?P<status>\w+).*?)?<bitrate>(?P<bitrate>[0-9]+)</bitrate>.*?(?P<base>rtmpe?://[^<]+).*?(?P<url>mp4:/[^<]+)(?=.*?(?P<sub>http://anytime.tv4.se/multimedia/vman/smiroot/[^<]+))?',
