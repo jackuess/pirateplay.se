7e171547868f703eee349980eaf953d6c4e925d2
Jacques de Laval <chucky@wrutschkow.org>
2013-01-07 21:09:18 +0100

    Shortcuts to latest release

--- lib/pirateplayer_downloader.py
+++ lib/pirateplayer_downloader.py
@@ -2,26 +2,22 @@ import cherrypy, os
 import sitemap
 
 logfile = 'data/pirateplayer_downloads.log'
+archive_dir = 'static/pirateplayer_archive'
 
 class PirateplayerDownloader():
+	def downloads_dict(self):
+		downloadlog_file = open(logfile, 'r')
+		downloadlog = dict([entry.split() for entry in downloadlog_file])
+		downloadlog_file.close()
+		return downloadlog
+		
 	@cherrypy.expose
 	@sitemap.add_to_sitemap('0.5')
 	@cherrypy.tools.genshi_template(filename='pirateplayer_downloads.html')
 	def index(self):
-		total_count = 0
-		out = '<table>'
-		downloadlog = open(logfile, 'r')
+		downloads = sorted(self.downloads_dict().items())
 		
-		#for entry in downloadlog:
-			#fn, count = entry.split()
-			#total_count += int(count)
-			#out += '<tr><td><a href="%s">%s</a></td><td>%s</td></tr>' % (fn, fn, count)
-		downloads = [entry.split() for entry in downloadlog]
-		
-		for d in downloads:
-			total_count += int(d[1])
-		
-		downloadlog.close()
+		total_count = sum(int(d[1]) for d in downloads)
 		
 		return { 'downloads': downloads,
 				'total_count': total_count }
@@ -29,12 +25,10 @@ class PirateplayerDownloader():
 	@cherrypy.expose
 	def default(self, *args, **kwargs):
 		filename = args[0]
-		path = os.path.join('static/pirateplayer_archive', filename)
+		path = os.path.join(archive_dir, filename)
 		
 		if os.path.exists(path):
-			downloadlog_file = open(logfile, 'r')
-			downloadlog = dict([entry.split() for entry in downloadlog_file])
-			downloadlog_file.close()
+			downloadlog = self.downloads_dict()
 			downloadlog[filename] = str(int(downloadlog.get(filename, '0')) + 1)
 			
 			downloadlog_file = open(logfile, 'w')
@@ -46,3 +40,26 @@ class PirateplayerDownloader():
 			raise cherrypy.InternalRedirect(os.path.join('../', path))
 		else:
 			raise cherrypy.NotFound()
+	
+	def redirect_to_latest(self, ext):
+		filenames = sorted(self.downloads_dict().keys())
+		for name in filenames:
+			if name.endswith(ext):
+				latest = name
+		
+		try:
+			raise cherrypy.HTTPRedirect(latest)
+		except UnboundLocalError:
+			raise cherrypy.NotFound()
+	
+	@cherrypy.expose
+	def latest_deb(self):
+		self.redirect_to_latest(ext='.deb')
+	
+	@cherrypy.expose
+	def latest_osx(self):
+		self.redirect_to_latest(ext='.dmg')
+	
+	@cherrypy.expose
+	def latest_win32(self):
+		self.redirect_to_latest(ext='.exe')
