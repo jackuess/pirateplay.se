881be1014a26d6997e6c9f9f3ccc3a20d132e0fc
Jacques de Laval <chucky@wrutschkow.org>
2013-02-04 17:00:09 +0100

    Subproccess exception handling

--- lib/pirateplay/lib/services/youtube.py
+++ lib/pirateplay/lib/services/youtube.py
@@ -1,10 +1,16 @@
 from ..rerequest import TemplateRequest
 import subprocess
 
+def youtube_dl(c):
+	try:
+		return subprocess.check_output(['youtube-dl', '-g', '--all-formats', '--get-format', c])
+	except subprocess.CalledProcessError:
+		return ''
+
 services = [{ 'title': 'Youtube', 'url': 'http://youtube.com/', 'feed_url': 'http://gdata.youtube.com/feeds/base/videos?alt=rss',
 			'items': [TemplateRequest(
 						re = r'(http://)?(www\.)?youtube\.com/(?P<url>.+)' ),
 					TemplateRequest(
 						re = r'(?P<final_url>.+?)\n\d+ - (?P<quality>\d+x\d+)\n',
-						decode_content = lambda c: subprocess.check_output(['youtube-dl', '-g', '--all-formats', '--get-format', c]),
-						encode_vars = lambda v: { } )] }]
+						decode_content = youtube_dl,
+						 )] }]#encode_vars = lambda v: { } )] }]
