27468b104d4dd1116f6bcaa5c6494897b2483eea
Jacques de Laval <chucky@wrutschkow.org>
2013-02-10 14:29:09 +0100

    Small fix

--- lib/pirateplay/lib/services/ur.py
+++ lib/pirateplay/lib/services/ur.py
@@ -5,7 +5,7 @@ services = [{ 'title': 'UR-play', 'url': 'http://urplay.se/', 'feed_url': 'http:
 					re = r'(http://)?(www\.)?(?P<domain>ur(play)?)\.se/(?P<req_url>.+)',
 					encode_vars = lambda v: { 'req_url': 'http://%(domain)s.se/%(req_url)s' % v } ),
 				TemplateRequest(
-					re = r'file_flash:\s+"(?P<final_url>[^"]+\.(?P<ext>mp[34]))".*?subtitles:\s+"(?P<subtitles>[^"]+)"',
+					re = r'file_flash:\s+"(?P<final_url>[^"]+\.(?P<ext>mp[34]))".*?subtitles:\s+"(?P<subtitles>[^",]+)',
 					encode_vars = lambda v: { 'final_url': ('rtmp://130.242.59.75/ondemand playpath=%(ext)s:/%(final_url)s app=ondemand' % v).replace('\\', ''),
 											'suffix-hint': 'flv',
 											'rtmpdump-realtime': True,
