c8588f1c1f8a49c1fda0733623054acc2c5543b6
Jacques de Laval <chucky@wrutschkow.org>
2013-03-14 14:59:51 +0100

    Dynamic screenshoot listing and changelog generation

--- lib/pirateplayer.py
+++ lib/pirateplayer.py
@@ -1,7 +1,13 @@
 import cherrypy
+from urllib2 import urlopen
 import sitemap
 import pirateplay
 from pirateplayer_downloader import PirateplayerDownloader
+
+def github_latest_version(username, repository):
+	import json
+	tags = json.load(urlopen('https://api.github.com/repos/%s/%s/tags' % (username, repository)))
+	return sorted(tags, key = lambda t: t.get('name', 'v0').split('.'))[-1].get('name', 'v0')
 
 class Pirateplayer():
 	@cherrypy.expose
@@ -14,7 +20,13 @@ class Pirateplayer():
 	@cherrypy.tools.genshi_template(filename='pirateplayer/changelog.html')
 	@sitemap.add_to_sitemap('0.5')
 	def changelog_html(self):
-		return {}
+		import markdown
+		from genshi.core import Markup
+		username = 'jackuess'
+		repository = 'pirateplayer'
+		tag = github_latest_version(username, repository)
+		tag = 'master'
+		md = unicode(urlopen('https://raw.github.com/%s/%s/%s/CHANGELOG.md' % (username, repository, tag)).read(), 'utf-8') return { 'changelog_markup': Markup(markdown.markdown(md)) }
 	
 	@cherrypy.expose
 	@cherrypy.tools.genshi_template(filename='pirateplayer/get.html')
@@ -27,7 +39,6 @@ class Pirateplayer():
 	@sitemap.add_to_sitemap('0.5')
 	def credits_html(self):
 		from json import load
-		from urllib2 import urlopen
 		
 		flattrs = load(urlopen('https://api.flattr.com/rest/v2/things/361015/flattrs?count=999'))
 		
@@ -37,6 +48,20 @@ class Pirateplayer():
 	@cherrypy.tools.genshi_template(filename='pirateplayer/screens.html')
 	@sitemap.add_to_sitemap('0.5')
 	def screens_html(self):
-		return {}
+		from os import listdir
+		
+		def create_description(img):
+			img = img.replace('.png', '')
+			img = img.replace('_', '.')
+			return img.replace('-', ' ')
+		
+		screen_dir = 'static/images/ppscreens/'
+		screens = sorted([{ 'thumb_link': cherrypy.url('../' + screen_dir + img.replace('.png', '.thumb.png')),
+						'link': cherrypy.url('../' + screen_dir + img),
+						'description': create_description(img) }
+					for img in listdir('static/images/ppscreens/')
+					if not img.endswith('.thumb.png')], reverse=True)
+		
+		return { 'screens': screens }
 	
 	downloads = PirateplayerDownloader()
