2ef5f29892b05e8ae951fd356fc9d4ed39cc56cb
Jacques de Laval <chucky@wrutschkow.org>
2013-03-21 09:45:41 +0100

    HLS-force for MTG

--- lib/pirateplay/lib/services/mtg.py
+++ lib/pirateplay/lib/services/mtg.py
@@ -6,7 +6,7 @@ mtg_encode = [lambda v: { 'req_url': 'http://viastream.viasat.tv/PlayProduct/%s'
 
 tv3play = { 'title': 'TV3-play', 'url': 'http://tv3play.se/', 'startvars': { 'suffix-hint': 'flv' },
 			'items': [TemplateRequest(
-						re = r'(http://)?(www\.)?tv[368]play\.se/.*(?:play/(?P<id>\d+)).*',
+						re = r'^(http://)?(www\.)?tv[368]play\.se/.*(?:play/(?P<id>\d+)).*',
 						encode_vars = mtg_encode[0]),
 						
 					TemplateRequest(
@@ -15,7 +15,7 @@ tv3play = { 'title': 'TV3-play', 'url': 'http://tv3play.se/', 'startvars': { 'su
 							
 mtg_alt = { 'startvars': { 'suffix-hint': 'flv' },
 			'items': [TemplateRequest(
-						re = r'(http://)?(www\.)?tv[368]play\.se/.*(?:play/(?P<id>\d+)).*',
+						re = r'^(http://)?(www\.)?tv[368]play\.se/.*(?:play/(?P<id>\d+)).*',
 						encode_vars = mtg_encode[0]),
 						
 					TemplateRequest(
@@ -24,9 +24,21 @@ mtg_alt = { 'startvars': { 'suffix-hint': 'flv' },
 					TemplateRequest(
 						re = r'<Url>(?P<final_url>[^<]+)',
 						encode_vars = mtg_encode[1])] }
+						
+mtg_hls = { 'items': [TemplateRequest(
+						re = r'(hls\+?)?(http://)?(www\.)?tv[368]play\.se/.*(?:play/(?P<id>\d+)).*',
+						encode_vars = lambda v: { 'req_url': 'http://viastream.viasat.tv/MobileStream/%(id)s' % v } ),
+					TemplateRequest(
+						re = r'"(?P<req_url>(?P<base>.+/)[^/]+.m3u8)"',
+						encode_vars = lambda v: { 'req_url': v['req_url'].replace('\\', ''), 'base': v['base'].replace('\\', '') } ),
+					TemplateRequest(
+						re = r'BANDWIDTH=(?P<bitrate>\d+)\n(?P<url>[^\n]+)',
+						encode_vars = lambda v: { 'final_url': '%(base)s%(url)s' % v,
+													'quality': '%s kbps' % (str(int(v['bitrate'])/1000)),
+													'suffix-hint': 'mp4' })] }
 
 #Dummies: TV6-play and TV8-play is caught by tv3play and mtg_alt
 tv6play = { 'title': 'TV6-play', 'url': 'http://tv6play.se/', 'feed_url': 'http://www.tv6play.se/rss/mostviewed' }
 tv8play = { 'title': 'TV8-play', 'url': 'http://tv8play.se/', 'feed_url': 'http://www.tv8play.se/rss/recent' }
 
-services = [tv3play, tv6play, tv8play, mtg_alt]
+services = [tv3play, tv6play, tv8play, mtg_alt, mtg_hls]
