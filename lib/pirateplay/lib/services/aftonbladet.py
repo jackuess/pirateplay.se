81877cc17c46d30dd7fd9d975ed02381c8c601a8
Jacques de Laval <chucky@wrutschkow.org>
2013-04-19 13:35:06 +0200

    Support for the new tv.aftonbladet.se

--- lib/pirateplay/lib/services/aftonbladet.py
+++ lib/pirateplay/lib/services/aftonbladet.py
@@ -4,7 +4,7 @@ video_re = r'playerWidth:\s(?P<width>\d+).*?playerHeight:\s(?P<height>\d+).*?vid
 
 rtmp = { 'title': 'Aftonbladet-TV', 'url': 'http://aftonbladet.se/', 'feed_url': 'http://www.aftonbladet.se/webbtv/rss.xml',
 		'items': [TemplateRequest(
-					re = r'(http://)?(www\.)?aftonbladet\.se/(?P<req_url>.+)',
+					re = r'^(http://)?(www\.)?aftonbladet\.se/(?P<req_url>.+)',
 					encode_vars = lambda v: { 'req_url': 'http://aftonbladet.se/%(req_url)s' % v } ),
 				TemplateRequest(
 					re = video_re % '(?P<base>rtmp://([^/]+)/[^/]+/)(?P<url>[^"]+)".*?videoIsLive:\s(?P<live>true|false)',
@@ -17,5 +17,19 @@ http = { 'items': [rtmp['items'][0],
 						re = video_re % '(?P<final_url>[^"]+)"',
 						encode_vars = lambda v: { 'quality': '%(width)sx%(height)s' % v,
 													'suffix-hint': 'flv' } )] }
+						
+tv_aftonbladet_se = { 'items': [TemplateRequest(
+								re = r'^(?P<req_url>(http://)(www\.)?tv.aftonbladet.se/.+)' ),
+							TemplateRequest(
+								re = r'data-aptomaId="(?P<data_id>[^"]+)"',
+								encode_vars = lambda v: { 'req_url': 'https://aftonbladet-play.drlib.aptoma.no/video/%(data_id)s.json' % v } ),
+							TemplateRequest(
+								re = r'"videoId":"(?P<video_id>[^"]+)"',
+								encode_vars = lambda v: { 'req_url': 'http://aftonbladet-play.videodata.drvideo.aptoma.no/actions/video/?id=%(video_id)s' % v } ),
+							TemplateRequest(
+								re = r'"bitrate":(?P<quality>\d+).*?"paths":\[{"address":"(?P<base>[^"]+)".*?"path":"(?P<path>[^"]+)".*?"filename":"(?P<filename>[^"]+)"',
+								encode_vars = lambda v: { 'final_url': 'http://%s/%s/%s'  % (v['base'], v['path'].replace('\\', ''), v['filename']),
+														'quality': '%(quality)s kbps' % v,
+														'suffix-hint': 'mp4' } )] }
 
-services = [rtmp, http]
+services = [rtmp, http, tv_aftonbladet_se]
